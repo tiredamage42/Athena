2e6ddd776253b5109b845cb759d8bf67a139bae0
Andres <andresgomezjr89@gmail.com>
2020-06-26 16:54:07 -0400

    made data creation save file every 100 iterations

--- weights_dataset.py
+++ weights_dataset.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     tf.disable_v2_behavior()
 
     # how many data points to generate
-    num_samples = 10
+    batch_amt = 100
 
     mnist = MNIST()
 
@@ -46,23 +46,23 @@ if __name__ == "__main__":
     session = tf.Session()
 
     # allocate the final dataset ahead of time
-    weights_dataset = np.zeros([num_samples, mnist.img_res_flat + 1, mnist.num_classes])
+    weights_dataset = np.zeros([batch_amt, mnist.img_res_flat + 1, mnist.num_classes])
     print ('\nDataset Size: {0} MB\n'.format(weights_dataset.nbytes/(1024.0 * 1024.0)))
 
-    def generate_trained_weights(iteration, batch_size=32, accuracy_test_frequency=100, min_accuracy=.9):
+    def generate_trained_weights(i, batch_size=32, accuracy_test_frequency=100, min_accuracy=.9):
         # reinitialize the weights
         session.run(initializer)
         acc = 0
-        i = 0
+        j = 0
         while acc < min_accuracy:
-            if i % accuracy_test_frequency == 0:
+            if j % accuracy_test_frequency == 0:
                 # test accuracy
                 acc = session.run(accuracy, feed_dict={
                     input: mnist.x_test,
                     target: mnist.y_test
                 })
                 
-            sys.stdout.write("\r{0}/{1} I:{2}:: Accuracy: {3:.1%} ==========".format(iteration+1, num_samples, i, acc))
+            sys.stdout.write("\r{0}/{1} I:{2}:: Accuracy: {3:.1%} ==========".format(i+1, batch_amt, j, acc))
             sys.stdout.flush()
 
             # get a batch of training data
@@ -73,26 +73,53 @@ if __name__ == "__main__":
                 input: input_batch,
                 target: labels_batch
             })
-            i+=1
+            j+=1
 
         # if we got here, model achieved at least 90% accuracy on test set
         final_weights, final_biases = session.run([weights, biases])
 
         # append the weights and biases as one matrix
-        weights_dataset[iteration, :-1] = final_weights
-        weights_dataset[iteration, -1] = final_biases
-        
-    print ("Training Datapoints:\n")
-    for i in range(num_samples):
-        generate_trained_weights(i)
+        weights_dataset[i, :-1] = final_weights
+        weights_dataset[i, -1] = final_biases
 
+    # save to npz file
+    def save_dataset():
+        if not os.path.exists('datasets/weights'):
+            os.makedirs('datasets/weights')
+
+
+        def check_and_rename(file_name, add=0):
+            original_file = file_name
+            if add != 0:
+                split = file_name.split(".")
+                part_1 = split[0] + "_" + str(add)
+                file_name = ".".join([part_1, split[1]])
+            if not os.path.isfile(file_name):
+                # reshape the dataset so it's in it's 'flat' form
+                np.savez_compressed(file_name, weights_dataset.reshape([-1, (mnist.img_res_flat + 1) * mnist.num_classes]))
+            else:
+                check_and_rename(original_file, add+1)
+
+        check_and_rename('datasets/weights/weights_dataset.npz')
+
+        
+    print ("\nTraining Datapoints (Press Ctrl-C To Quit):\n")
+
+    # infinite loop, every 100 iterations, save the data to a file
+    try:
+        i = 0
+        while True:
+            generate_trained_weights(i)
+            i+=1
+            if i % batch_amt == 0:
+                i = 0
+                save_dataset()
+
+    except KeyboardInterrupt:
+        pass
+
+    print ("\nClosing tensorflow session")
     # cleanup tensorflow resources
     session.close()
 
-    # reshape the dataset so it's in it's 'flat' form
-    weights_dataset = weights_dataset.reshape([num_samples, (mnist.img_res_flat + 1) * mnist.num_classes])
-
-    # save to npz file
-    if not os.path.exists('datasets/'):
-        os.makedirs('datasets/')
-    np.savez_compressed('datasets/weights_dataset.npz', weights_dataset)
+    
